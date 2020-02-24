36deb5f7092d57ba5796df880a11d8699ee6ca53
Qoqa Printing Pi <info@fullrange.ch>
2020-02-24 10:25:14 +0100

    Update at QoQa

--- printing/printTicket.sh
+++ printing/printTicket.sh
@@ -1,4 +1,7 @@
-node dmxled.js &
-wget http://10.0.1.175:8080/api/render/random
+# node dmxled.js &
+# wget http://10.0.1.175:8080/api/render/random
+# node dmxled.js &
 lp -d EPSON_TM-T20II random
-rm -rf random
+./downloadTicket.sh
+# sleep 1
+# rm -rf random
