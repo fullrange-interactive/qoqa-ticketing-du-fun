d38112525979747f5d0eb1403c4aacd8678a7aad
floriansegginger <florian@fullrange.ch>
2020-02-24 09:42:12 +0100

    Update with led working

--- printing/main.js
+++ printing/main.js
@@ -1,26 +1,33 @@
 const exec = require('child_process').exec;
 const Gpio = require('onoff').Gpio;
 
-const button = new Gpio(2, 'in', 'falling', {debounceTimeout: 10});
+const button = new Gpio(2, 'in', 'falling', {debounceTimeout: 30});
 const buttonBaseOutput = new Gpio(3, 'out');
 const led = new Gpio(4, 'out');
 
 buttonBaseOutput.writeSync(0);
+led.writeSync(1);
 
 button.watch((err, value) => {
 
+  const iv = setInterval(_ => led.writeSync(led.readSync() ^ 1), 200);
+
   console.log("Button pressed !");
   exec("./printTicket.sh", // command line argument directly in string
-  function (error, stdout, stderr) {      // one easy function to capture data/errors
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-});
+    function (error, stdout, stderr) {      // one easy function to capture data/errors
+
+      clearInterval(iv);
+      led.writeSync(1);
+
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+
+  });
 
 });
 
 console.log("Running qoqa-printing-service");
 
-//const iv = setInterval(_ => led.writeSync(led.readSync() ^ 1), 200);
